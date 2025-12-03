cf3833298d72325bd7e6dedd2138206bff11fe98
Ablasse Kingcaid-Ouedraogo <ablassecode@outlook.com>
2025-12-03 00:54:10 -0600

    Added API Files
    A  PomoDojo_API.h
    A  PomoDojo_Exports.cpp
    A  states/profile.cs
    A  states/timer.cs

---
+++ PomoDojo_API.h
@@ -0,0 +1,14 @@
+#ifdef POMODOJO_EXPORTS
+#define POMODOJO_API _declspec(dllexport)
+#else
+#define POMODOJO_API _declspec(dllimport)
+#endif
+
+extern "C" {
+	POMODOJO_API void StartPomodojo(int workMinutes, int breakMinutes);
+	POMODOJO_API void StopPomodojo();
+	POMODOJO_API int GetRemainingSeconds();
+	POMODOJO_API bool IsWorkPeriod();
+
+	
+}
